cd8e35a5d9078dc638772d6fdfa623a33cfce4ad
lonaking <lonaking@foxmail.com>
2015-12-20 14:11:25 +0800

    init

---
+++ src/main/java/com/pirecing/web/base/response/ResponseCode.java
@@ -0,0 +1,5 @@
+package com.pirecing.web.base.response;
+
+public class ResponseCode {
+	public static final int CODE_OK = 200;
+}
---
+++ src/main/java/com/pirecing/web/base/response/ResponseMsg.java
@@ -0,0 +1,7 @@
+package com.pirecing.web.base.response;
+
+public class ResponseMsg {
+	public static final String MSG_SUCCESS = "success";
+	public static final String MSG_ERROR = "error";
+
+}
